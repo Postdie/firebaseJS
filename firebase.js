762c86c8b6f001e9421cb671d0cb3cb4d3f79c1b
franlops <franlopbri@gmail.com>
2022-11-16 10:39:03 -0600

    Se implementa la recuperacion de datos onSnapShot

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { collection, getFirestore, addDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+import { collection, getFirestore, addDoc, getDocs, 
+        onSnapshot } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
 export const saveTask = (title, description) => addDoc(collection(db, 'tasks'), { title, description });
+
+export const getTasks = () => getDocs(collection(db, 'tasks'));
+
+export const onGetTasks = callback => onSnapshot(collection(db, 'tasks'), callback);
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import { saveTask } from './firebase.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('works!');
-});
+import { getTasks, saveTask, onGetTasks } from './firebase.js';
 
 const formTask = document.querySelector('#task-form');
+const taskContainer = document.querySelector('#tasks-container');
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+    onGetTasks(querySnapshot => {
+        if(taskContainer.firstChild)
+            taskContainer.removeChild(taskContainer.firstChild)
+        const div = document.createElement('div');
+        let html = '<ul>';
+        querySnapshot.forEach(doc => {
+            const task = doc.data();
+            html += `<li>${task.title} - ${task.description}</li>`;
+        });
+        html += `</ul>`;
+        div.innerHTML = html;
+        taskContainer.appendChild(div);
+    });
+
+});
 
 formTask.addEventListener('submit', (e) => {
     e.preventDefault();
